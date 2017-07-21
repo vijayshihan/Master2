bcdeb12d17951441f1f063e63274b8b99d9aa80a
vijayshihan <shindevs87@gmail.com>
2017-07-21 05:34:31 -0700

    first checkin

---
+++ src/main/java/MainJava/MainJavaExample.java
@@ -0,0 +1,10 @@
+package MainJava;
+
+public class MainJavaExample {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("This is main Method");
+	}
+
+}
---
+++ src/test/java/Test/TestProgram.java
@@ -0,0 +1,12 @@
+package Test;
+
+import org.testng.annotations.Test;
+
+public class TestProgram {
+
+	@Test 
+	public static void TestCase1()
+	{
+		System.out.println("This js simple program test case 1");
+	}
+}
