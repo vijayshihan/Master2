32d741e61d051fd668a7596f00f360b220128e6b
vijayshihan <shindevs87@gmail.com>
2017-07-21 05:42:10 -0700

    second change

--- src/main/java/MainJava/MainJavaExample.java
+++ src/main/java/MainJava/MainJavaExample.java
@@ -4,7 +4,9 @@ public class MainJavaExample {
 	
 	public static void main(String[] args) {
 		
+		System.out.println("This is main Method2");
 		System.out.println("This is main Method");
+		System.out.println("This is main Method3");
 	}
 
 }
